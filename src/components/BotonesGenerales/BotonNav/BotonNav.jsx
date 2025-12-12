379b72ac3e7b97fa44fe4f9bafa4ef7926f7f2e7
Carolina <aizenstatcarolina@gmail.com>
2025-12-11 22:45:54 -0300

    menu

--- src/components/BotonesGenerales/BotonNav/BotonNav.jsx
+++ src/components/BotonesGenerales/BotonNav/BotonNav.jsx
@@ -1,26 +1,21 @@
+// src/components/BotonesGenerales/BotonNav/BotonNav.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './BotonNav.module.css';
 
 const BotonNav = ({ to, imgSrc, altText, end = false }) => {
 
-    const iconStyle = {
-        width: '32px',
-        height: '32px',
-        objectFit: 'contain',
-        transition: 'filter 0.3s ease'
-    };
-
-    const linkClassName = ({ isActive }) => isActive ? styles.navLinkActive : undefined;
-
     return (
-
         <NavLink
             to={to}
             end={end}
-            className={`${styles.navLink} ${linkClassName({ isActive: false })}`}
+            className={({ isActive }) => 
+                // Si está activo, suma las dos clases: .navLink Y .navLinkActive
+                `${styles.navLink} ${isActive ? styles.navLinkActive : ''}`
+            }
         >
-            <img src={imgSrc} alt={altText} style={iconStyle} />
+            {/* Quitamos los estilos inline para que el CSS controle el tamaño y color */}
+            <img src={imgSrc} alt={altText} />
         </NavLink>
     );
 };
